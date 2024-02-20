58737ddc8b6f123c6c51cb68c12b684c81e017bc
jayden.d.f <56249678+JaydenDF@users.noreply.github.com>
2024-02-20 22:45:34 +0100

    startGridSystem

---
+++ Assets/ScriptJay/GridTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTest : MonoBehaviour
+{
+    public void Awake()
+    {
+        GridSystem grid = new GridSystem(4, 2);
+    }
+
+    public void Update()
+    {
+        
+    }
+}
